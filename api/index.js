bb9c4f31343f202dbfda3b6e4503365579241635
halareka <149045655+halareka@users.noreply.github.com>
2024-05-07 23:17:06 +0700

    fix: server work

--- api/index.js
+++ api/index.js
@@ -1,7 +1,39 @@
 const express = require("express");
 const app = express();
+const fs = require('fs');
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+var bodyParser = require('body-parser')
+
+
+
+// parse application/x-www-form-urlencoded
+var jsonParser = bodyParser.json()
+
+app.use(express.static('public'));
+
+app.get("/", (req, res) => res.render("index"));
+app.get("/chat", (req, res) => res.render('chat')); 
+
+
+app.post('/send_msg', jsonParser, (req, res) => {
+    const message = req.body.message;
+
+
+    res.status(200).send({
+      res: 'successful',
+      restwo: 'hello',
+      resthree: message
+    });
+  });
+
+app.post('/get_chat', (req, res) => {
+    // const fileContent = fs.readFileSync('db/test.txt', 'utf-8');
+    // const messages = fileContent.split('\n').filter(msgs => msgs.trim());
+    let messages = [1,10,100,1000,10000,2,20,200,2000,'helloo']
+    res.status(200)
+    res.send({ res : messages });
+});
+
 
 app.listen(3001, () => console.log("Server ready on port 3000."));
 
