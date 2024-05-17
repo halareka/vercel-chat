3eb1ed4d18da0dcf69855bb96d0bd36f6e402c9b
halareka <149045655+halareka@users.noreply.github.com>
2024-05-17 23:42:34 +0700

    feat : successful for deploy

--- api/index.js
+++ api/index.js
@@ -1,29 +1,19 @@
+require('dotenv').config()
 const express = require("express");
 const app = express();
 const mysql = require("mysql")
 
-const conn = mysql.createConnection({
-  host:"sql6.freemysqlhosting.net",
-  user:"sql6705815",
-  database:"sql6705815",
-  password:"A6nasm2rgr",
-  charset: "utf8"
-})
 const pool = mysql.createPool({
     connectionLimit: 100,
-    host:"sql6.freemysqlhosting.net",
-    user:"sql6705815",
-    database:"sql6705815",
-    password:"A6nasm2rgr",
-    charset: "utf8"
+    host:       process.env.HOST,
+    user:       process.env.USER,
+    database:   process.env.DATABASE,
+    password:   process.env.PASSWORD,
+    charset:    "utf8"
 })
 
 var bodyParser = require('body-parser')
-
-let a = [];
-
 var jsonParser = bodyParser.json()
-
 app.use(express.static('public'));
 
 app.get("/", (req, res) => res.render("index"));
@@ -33,7 +23,7 @@ app.get("/.well-known/vercel/flags",(req,res) => res.send('yaapi)))'))
 app.post('/send_msg', jsonParser, (req, res) => {
     let message = req.body.message;
     pool.getConnection((error, connection) => {
-        if (error) throw error;
+        if (error) throw error;                                                                                                                                                                                                
         connection.query('INSERT INTO `chat`(`chat`) VALUES (?)', [message], err => {
             if (err) throw err;
             res.status(200).send({
@@ -44,6 +34,7 @@ app.post('/send_msg', jsonParser, (req, res) => {
         });
     });
 });
+
 app.post('/get_chat', (req, res) => {
     pool.getConnection((error, connection) => {
         if (error) throw error;
@@ -53,7 +44,7 @@ app.post('/get_chat', (req, res) => {
             let a = [];
             b.forEach(element => {
                 a.push(element.chat);
-            });
+            });                             
             res.status(200).send({
                 res: a                                                                                       
             });
@@ -63,7 +54,31 @@ app.post('/get_chat', (req, res) => {
     });
 });
 
-
+app.post('/get_length', (req, res) => { 
+    pool.getConnection((error, connection) => {
+        if (error) throw error;
+        connection.query('SELECT COUNT(*) FROM `chat`'  ,(err,result) => {
+            if (err) throw err;
+            let b = result;                           
+            res.status(200).send({
+                res: b                                                                                      
+            });
+            connection.destroy();
+        });
+    });
+});
+app.post('/del_msg', (req, res) => { 
+    pool.getConnection((error, connection) => {
+        if (error) throw error;
+        connection.query('DELETE FROM `chat` LIMIT 1'  ,(err,result) => {
+            if (err) throw err;                   
+            res.status(200).send({
+                res: 'successful delete'                                                                                     
+            });
+            connection.destroy();
+        });
+    });
+});
 app.listen(4000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
---
+++ public/script.js
@@ -0,0 +1,150 @@
+
+let lastLength = 0;
+let arrayOfChat = [];
+window.onload =  () => {
+    getMessage()
+    freezeButton()
+    window.scrollTo({
+        top: 100,
+        behavior: "smooth"
+    });
+    if(localStorage.getItem('nick')){
+        document.getElementById('nickname').value = localStorage.getItem('nick')
+    }else{
+        localStorage.setItem('nick' , 'Аноним')
+        document.getElementById('nickname').value = 'Аноним'
+    }
+    document.getElementById('welc').innerText = `Привет , ${localStorage.getItem('nick')} !`
+}
+//nickname_autofill
+function updateNickname(){
+    let nick = document.getElementById('nickname').value;
+    localStorage.setItem('nick' , nick )
+}
+function freezeButton(){
+    let btn = document.getElementById('send');
+    btn.disabled = true;
+    let count = 0;
+    let intervalId = setInterval(()=>{
+        count++;
+        btn.innerText = `Подождите... ${count}`
+        count == 7 ? unfreezeButton() : false;
+    },1000) 
+    function unfreezeButton(){
+        clearInterval(intervalId)
+        btn.disabled = false;
+        btn.innerText = "Отправить";
+    }
+}
+function addMessage(){
+    let inp = document.getElementById('message');
+    if(!inp.value){
+        setTimeout(()=> inp.style.background = '#FFFFFF', 1000)
+        inp.style.background = '#ffb3b3'
+    }else{
+        if(inp.value.length > 200){
+            alert('слишком длинное сообщение!!!у нас и так база данных на 5 мегабайт!(')
+            inp.value = '';
+            return 0;
+        }
+        let valueOnInput = `<b>${localStorage.getItem('nick')}</b>:<br>${inp.value}`;
+        // console.log(valueOnInput)
+        inp.value = ''
+        freezeButton()
+        sendMessage(valueOnInput)
+    }
+}
+function sendMessage(value){
+    fetch('/send_msg', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            message: value
+        })
+    })
+    .then(response => response.json())
+    .then(data => { console.log(data)})
+    .catch(error =>{console.error('Ошибка:', error);});
+}
+function getMessage(val){
+    fetch('/get_chat', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            msg: 'send_chat'
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        arrayOfChat = data.res;
+        lastLength = arrayOfChat.length ;
+        if(val){
+            let newArr = [];
+            for(let i = 1; i < val+1 ; i++){
+                newArr.push(arrayOfChat[arrayOfChat.length-i])
+            }
+            arrayOfChat = newArr;
+        }
+        // console.log(arrayOfChat)
+        revert(arrayOfChat);
+    })
+    .catch(error => {
+        console.error('Ошибка:', error);
+    });
+
+}
+function revert(arr){
+    
+    arr.forEach(el => {
+        const chatElement = `<li class="message"><span class="content">${el}</span></li>`;
+        document.getElementById('messages').innerHTML += chatElement;
+    });
+}
+
+setInterval(updateCli , 10000)
+
+function updateCli(){
+    // console.log(lastLength + '<--- длина на клиенте')
+    fetch('/get_length', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        lenOfChat = data.res[0]['COUNT(*)'];
+        // console.log(lenOfChat + '<--- длина на сервере')
+        if(lenOfChat > 500){
+            deleteLastMsg()
+        }
+        if(lenOfChat > lastLength){
+            getMessage(lenOfChat-lastLength);
+        }else {
+            return 0 ;
+        }
+    })
+    .catch(error => {
+        console.error('Ошибка:', error);
+    });
+}
+function deleteLastMsg(){
+    fetch('/del_msg', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        getMessage(); // Update the chat after deleting the message
+    })
+    .catch(error => {
+        console.error('Ошибка:', error);
+    });
+}
